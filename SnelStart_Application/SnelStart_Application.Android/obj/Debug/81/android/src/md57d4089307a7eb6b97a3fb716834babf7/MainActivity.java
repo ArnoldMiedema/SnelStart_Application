1063c2f9cd4e4eb6b6329bab5b266be7721e5f45
Serenitytwo <Arnold.miedema@gmail.com>
2018-12-09 22:19:32 +0100

    Initial commit

---
+++ SnelStart_Application/SnelStart_Application.Android/obj/Debug/81/android/src/md57d4089307a7eb6b97a3fb716834babf7/MainActivity.java
@@ -0,0 +1,47 @@
+package md57d4089307a7eb6b97a3fb716834babf7;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("SnelStart_Application.Droid.MainActivity, SnelStart_Application.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("SnelStart_Application.Droid.MainActivity, SnelStart_Application.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ SnelStart_Application/SnelStart_Application/Register.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SnelStart_Application
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Register : ContentPage
+	{
+		public Register ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void Register_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(Username.Text) && (!string.IsNullOrWhiteSpace(Password1.Text) && !string.IsNullOrWhiteSpace(Password2.Text) && Password1.Text == Password2.Text) )
+            {
+                //add database
+                await Navigation.PushAsync(new Login());
+            }
+        }
+    }
+}
